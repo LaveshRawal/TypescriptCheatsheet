c6f8863780fbcb91b3f71385f3ca808954f54fff
PREM KUMAR <premenaga@gmail.com>
2022-06-30 16:10:34 +0530

    ts cheatsheet

---
+++ index.js
@@ -0,0 +1,69 @@
+var id = 5;
+var fname = "Max";
+var isCool = true;
+var x = [1, true, 3];
+//tuple
+var tuple = [1, "Max"];
+// tuple array
+var tupleArray = [
+    [1, "Max"],
+    [2, "Manu"],
+    [3, "Max"],
+];
+//union
+var id1 = 5;
+//enums
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "UP";
+    Direction2["Down"] = "DOWN";
+    Direction2["Left"] = "LEFT";
+    Direction2["Right"] = "RIGHT";
+})(Direction2 || (Direction2 = {}));
+//Objects
+var user = {
+    name: "Max",
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
+};
+var user1 = {
+    name: "Max",
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
+};
+// type assertion
+var cid = 1;
+var customerIdasNum = cid;
+//functions
+function addNum(x, y) {
+    return x + y;
+}
+// void type for no return value
+function log(message) {
+    console.log(message);
+}
+var user2 = {
+    name: "Max",
+    age: 30
+};
+var p1 = 1;
+var add = function (x, y) {
+    return x + y;
+};
+// classes
+var Person = /** @class */ (function () {
+    function Person(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    return Person;
+}());
+var prem = new Person(1, "Prem Kumar");
+var prem1 = new Person(2, "kumar kumar");
---
+++ index.ts
@@ -0,0 +1,118 @@
+let id: number = 5;
+let fname: string = "Max";
+let isCool: boolean = true;
+let x: any = [1, true, 3];
+
+//tuple
+let tuple: [number, string] = [1, "Max"];
+
+// tuple array
+
+let tupleArray: [number, string][] = [
+  [1, "Max"],
+  [2, "Manu"],
+  [3, "Max"],
+];
+
+//union
+
+let id1: number | string = 5;
+
+//enums
+
+enum Direction1 {
+  Up = 0,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = "UP",
+  Down = "DOWN",
+  Left = "LEFT",
+  Right = "RIGHT",
+}
+
+//Objects
+
+const user: {
+  name: string;
+  age: number;
+  hobbies: string[];
+} = {
+  name: "Max",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+};
+
+// with type
+
+type user1 = {
+  name: string;
+  age: number;
+  hobbies: string[];
+};
+
+const user1 = {
+  name: "Max",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+};
+
+// type assertion
+let cid: any = 1;
+let customerIdasNum = cid as number;
+
+//functions
+
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+
+// void type for no return value
+function log(message: string | number): void {
+  console.log(message);
+}
+
+//interfaces || custom types
+
+interface User {
+  readonly name: string; // readonly means that the property can only be read, not written
+  age: number;
+  experience?: number; //add? for optional property
+}
+
+const user2: User = {
+  name: "Max",
+  age: 30,
+};
+
+// customtype
+
+type point = number | string;
+
+const p1: point = 1;
+
+// interfaces with functions
+
+interface addFunction {
+  (x: number, y: number): number;
+}
+
+const add: addFunction = (x: number, y: number): number => {
+  return x + y;
+};
+
+// classes
+
+class Person {
+  private id: number; // private means that the property can only be read, not written
+  name: string;
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
+const prem = new Person(1, "Prem Kumar");
+const prem1 = new Person(2, "kumar kumar");
